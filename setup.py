c9362660355e762739ee04f9baa9dfb10a4d73ce
bvandercar-vt <blake.vandercar@validatek.com>
2025-01-27 23:22:19 -0700

    underscore

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-   name='serato-tags',
+   name='serato_tags',
    version='1.0',
    description='Serato track metadata tags',
    author='TODO',
    author_email='TODO',
    packages=find_packages(where="scripts"),
-   package_dir={'serato-tags':'scripts'},
+   package_dir={'serato_tags':'scripts'},
    install_requires=['mutagen']
 )
