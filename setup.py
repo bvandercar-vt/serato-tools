1b9ff8e9049111e4df8daef8aaee86499beaf951
bvandercar-vt <blake.vandercar@validatek.com>
2025-01-28 02:58:34 -0700

    init files

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_namespace_packages, find_packages, setup
+from setuptools import find_packages, setup
 
 setup(
    name='serato_tags',
@@ -6,7 +6,7 @@ setup(
    description='Serato track metadata tags',
    author='TODO',
    author_email='TODO',
-   packages=find_namespace_packages(),   
+   packages=find_packages(),   
    package_dir={"serato_tags": "scripts"},
    install_requires=['mutagen'],
    extras_require = {
