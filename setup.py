a5cea0f7962c8e47a9e0d87c6a88329f12411aaf
bvandercar-vt <blake.vandercar@validatek.com>
2025-01-27 22:46:08 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='serato-tags',
+   version='1.0',
+   description='Serato track metadata tags',
+   author='TODO',
+   author_email='TODO',
+   packages=['serato-tags'],
+   install_requires=[],
+)
